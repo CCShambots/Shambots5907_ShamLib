753df9d8d646e6f6d45645ec84ca79b3cbd07292
J-Barta <53406552+J-Barta@users.noreply.github.com>
2024-04-29 17:22:05 -0400

    comment silly sensor stuff

--- sensor/ThroughBoreEncoder.java
+++ sensor/ThroughBoreEncoder.java
@@ -10,11 +10,22 @@ public class ThroughBoreEncoder {
   private final double offset;
   private boolean invert = false;
 
+  /**
+   * REV Robotics through bore encoder wrappper.
+   * WARNING: DO NOT trust the values this returns instantly after it is instantiated.
+   * Because it is a digital duty cycle, it takes a non-zero amount of time for the reading to stabilize to the correct value. 
+   * @param port Digital port of the encoder
+   * @param offsetDegrees Degrees to offset the encoder reading by. Should be determined by using the value the encoder reads when at the mechanisms "zero" position
+   */
   public ThroughBoreEncoder(int port, double offsetDegrees) {
     this.cycle = new DutyCycle(new DigitalInput(port));
     this.offset = offsetDegrees / 360.0;
   }
 
+  /**
+   * Enable or disable inversion on the encoder
+   * @param value true or false
+   */
   public void setInverted(boolean value) {
     invert = value;
   }
